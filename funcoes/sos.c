3533d4b9a67a9cbe1f12eaa0a00a6d8812b22c85
Matheus Soares Ladeia Alves <matheus-ladeia@hotmail.com>
2025-01-18 22:38:06 -0300

    Tornando a função blink genérica para qualquer LED

--- funcoes/sos.c
+++ funcoes/sos.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-int LED_PIN 13 
-
 #define gap 125    
 #define ponto 200  
 #define traco 800  
 #define letras 250 
                     
-                            
-void sinal();                                    
-void blink_led(uint tempo);                     
-
-
 void sinal() {
     blink_led(ponto);
     sleep_ms(gap);
@@ -37,8 +30,8 @@ void sinal() {
     sleep_ms(500);
 }
 
-void blink_led(uint tempo) {
-    gpio_put(LED_PIN, true);
+void blink_led(uint LED, uint tempo) {
+    gpio_put(LED, true);
     sleep_ms(tempo);
-    gpio_put(LED_PIN, false);
+    gpio_put(LED, false);
 }
