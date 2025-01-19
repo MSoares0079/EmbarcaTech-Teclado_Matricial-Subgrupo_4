4f220b7b44e3afdfa063354e8fb4bca149807153
Matheus Soares Ladeia Alves <matheus-ladeia@hotmail.com>
2025-01-19 19:58:37 -0300

    Adicionando mais funções envolvendo o buzzer

--- Teclado_Matricial.c
+++ Teclado_Matricial.c
@@ -5,6 +5,7 @@
 #include "funcoes/Soma_BIN.c"
 #include "funcoes/PlaySong.c"
 #include "funcoes/sinal.c"
+#include "funcoes/apagar_led.c"
 
 #define ROWS 4  // Número de linhas
 #define COLS 4  // Número de colunas
@@ -69,36 +70,53 @@ int main() {
             if(key == 'A'){
                gpio_put(LED_VERDE,true);
                printf("Led verde aceso\n");
-            }
-            else if(key == 'D'){
-              play_note(BUZZER, 440,1000);//Toca a nota A4(440 Hz) por 1s
-              printf("Buzzer tocando a nota Lá(440Hz) por 1s\n");
-            } else if (key == '1') {
-                piscaBin_led(LED_VERDE, LED_AZUL, LED_VERMELHO);
+            }else if(key == 'B'){
+                gpio_put(LED_AZUL,true);
+                printf("Led azul aceso\n");
+            }else if(key == 'C'){
+                gpio_put(LED_VERMELHO,true);
+                printf("Led vermelho aceso\n");
+            }else if (key == 'D') {
+                printf("Tocando a sequência: Dó, Ré, Mi, Fá, Sol, Lá, Si, Dó\n");
+                play_song(BUZZER); 
+            }else if (key == '1') {
                 printf("Leds piscando no formato de soma binaria\n");
+                piscaBin_led(LED_VERDE, LED_AZUL, LED_VERMELHO); 
             } else if (key == '2'){
                 play_note(BUZZER, 262, 1000);
                 gpio_put(LED_VERMELHO, true);
                 gpio_put(LED_AZUL, true);
                 printf("Buzzer tocando a nota Dó e leds azul e vermelho ligados");
-            }
-            else if (key == '0') {
-                printf("Tocando a sequência: Dó, Ré, Mi, Fá, Sol, Lá, Si, Dó\n");
-                play_song(BUZZER); 
-            } else if (key == 'B'){
+            }else if(key == '3'){
+              play_note(BUZZER, 262,1000);//Toca a nota Dó
+              printf("Buzzer tocando a nota Dó(262Hz) por 1s\n");
+            }else if(key == '4'){
+              play_note(BUZZER, 294,1000);//Toca a nota Ré
+              printf("Buzzer tocando a nota Ré(294Hz) por 1s\n");
+            }else if(key == '5'){
+              play_note(BUZZER, 330,1000);//Toca a nota Mi
+              printf("Buzzer tocando a nota Mi(330Hz) por 1s\n");
+            }else if(key == '6'){
+              play_note(BUZZER, 349,1000);//Toca a nota Fá
+              printf("Buzzer tocando a nota Fá(349Hz) por 1s\n");
+            }else if(key == '7'){
+              play_note(BUZZER, 392,1000);//Toca a nota Sol
+              printf("Buzzer tocando a nota Sol(349Hz) por 1s\n");
+            }else if(key == '8'){
+              play_note(BUZZER, 440,1000);//Toca a nota Lá
+              printf("Buzzer tocando a nota Lá(440Hz) por 1s\n");
+            }else if(key == '9'){
+              play_note(BUZZER, 494,1000);//Toca a nota Si
+              printf("Buzzer tocando a nota Si(494Hz) por 1s\n");
+            }   
+             else if (key == '#'){
                 printf("Enviando código SOS...\n");
                 sinal(LED_VERMELHO);
-            } else if(key == '4'){
-                gpio_put(LED_VERMELHO,true);
-                printf("Led vermelho aceso\n");
-            } else if(key == '5'){
-                gpio_put(LED_AZUL,true);
-                printf("Led azul aceso\n");
-            } else if(key == '3'){
+            }else if(key == '*'){
                 apagar_led(LED_VERDE);
                 apagar_led(LED_VERMELHO);
                 apagar_led(LED_AZUL);
-            } else{
+            }else{
                 printf("Nenhuma funcionalidade foi atribuida a essa tecla\n");
                }
             } 
